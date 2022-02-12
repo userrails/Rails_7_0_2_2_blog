ef52f9026e49c20505a0b4a679b33e02a7caaf6e
Shiv Raj Badu <shivrajbadu@gmail.com>
2022-02-13 00:12:58 +0545

    basic auth added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "siv", password: "Secret1", only: [:index, :show]
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "siv", password: "Secret1", only: :destroy
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
