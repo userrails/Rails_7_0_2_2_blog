63e240f23d59878136b270a79cf7f1ddf6d4a8c5
Shiv Raj Badu <shivrajbadu@gmail.com>
2022-02-13 00:08:57 +0545

    article/comment status added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,17 @@ class CommentsController < ApplicationController
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
     end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article), status: 303
+    end
     
     private
 
     def comment_params
-        params.require(:comment).permit(:commenter, :body)
-    end    
+        params.require(:comment).permit(:commenter, :body, :status)
+    end
 end
