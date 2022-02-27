b4dcf58adaf8be949918c8f5b882d655e003e2fe
Shiv Raj Badu <shivrajbadu@gmail.com>
2022-02-27 16:19:13 +0545

    display copy to clipboard action button for supporting browsers and else hide that for unsupporting browsers

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -9,15 +9,36 @@ export default class extends Controller {
   // `this.sourceTargets` evaluates to an array of all `source` targets in the controller's scope.
   // `this.hasSourceTarget` evaluates to `true` if there is `source` target or `false` if not.
   static targets = ["source"]
+  // add `data-clipboard-supported-class` attribute in this controller as static class which will
+  // control the specific CSS class in HTML, and controller becomes more easily adaptable to different
+  // CSS approaches. The specific class added like this can be accessed via `this.supportedClass`.
+  static classes = [ "supported" ]
 
+  // connect() method adds the class name to the controller's element when the user agent has permission
+  // to write to the Clipboard.
   connect() {
     console.log("Namaste!")
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+      if (result.state === 'granted') {
+        this.element.classList.add(this.supportedClass);
+      }
+    });
   }
 
   // select the input field’s contents and call the clipboard API
   copy(event) {
     event.preventDefault()
-    let el = navigator.clipboard.writeText(this.sourceTarget.value)
-    console.log('copied', this.sourceTarget.value)
+    // Solution 1
+    // let el = navigator.clipboard.writeText(this.sourceTarget.value)
+    // console.log('copied', this.sourceTarget.value)
+
+    // Solution 2
+    this.sourceTarget.select()
+    document.execCommand("copy")
   }
 }
+
+// Steps to test:
+// Disable JS in browser, reload the page, and notice the Copy button is no longer visible.
+// PIN field is enhanced: Copy button's baseline state is hidden, becoming visible only when
+// our JavaScript detects support for the Clipboard API.
