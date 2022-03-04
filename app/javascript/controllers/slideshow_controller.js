df7d9d84bf8eb851d6e570e64c4a24025ee0e7a0
Shiv Raj Badu <shivrajbadu@gmail.com>
2022-03-04 17:39:30 +0545

    slider example with adv. functions

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -3,25 +3,43 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "slide" ]
+  // static values = { index: Number } // `static values = { index: Number }` is Typed value controller properties, this is static property called as values.
+  // Syntax: `data-{controller-name}-{define-valPropertyYouLike}-name` and is called as `this.valPropertyYouLikeValue`
 
-  initialize() {
-    this.index = 0
-    this.showCurrentSlide()
-  }
 
+  static values = { index: { type: Number, default: 2 } } // we can also set default value so that if index data attribute is missed in controller element, it will take from default value.
+
+  // static values = { index: Number, effect: { type: String, default: "kenburns" } }
+
+  // initialize() {
+  //   // this.index = Number(this.element.dataset.index) // since we had data-index="1" in DOM
+  //   // ^ To get rid with manual type converstion, let's use controller property associated with attribute so that type conversion will be automated.
+  //   // In addition, Stimulus will create `this.indexValue` controller property associated with a `data-slideshow-index-value` attribute and it handles numeric conversion for us.
+  //   // console.log(this.indexValue) // 1
+  //   // console.log(typeof this.indexValue) // number
+
+  //   // this.indexValue
+  //   this.showCurrentSlide()
+  // }
+
+  // `this.showCurrentSlide()` we event don't need because Stimulus have method `indexValueChanged()` to initialize and in response to any change in the `data-slideshow-index-value` data value attribute.
   next() {
-    this.index++
-    this.showCurrentSlide()
+    this.indexValue++
+    // this.showCurrentSlide()
   }
 
   previous() {
-    this.index--
+    this.indexValue--
+    // this.showCurrentSlide()
+  }
+
+  indexValueChanged() { // inbuilt method
     this.showCurrentSlide()
   }
 
   showCurrentSlide() {
     this.slideTargets.forEach((element, index) => { // loop through all targets and compare if their indexing postion is same as assigned index var
-      element.hidden = (index != this.index)
+      element.hidden = (index != this.indexValue)
     })
   }
 }
