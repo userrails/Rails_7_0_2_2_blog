f46c125fb1c7743f2fe00191758e54206dcead67
Shiv Raj Badu <shivrajbadu@gmail.com>
2022-03-12 18:16:47 +0545

    when page link is clicked that page content is loaded asynchronously

--- app/javascript/controllers/content_loader_controller.js
+++ app/javascript/controllers/content_loader_controller.js
@@ -3,35 +3,46 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = { url: String, refreshInterval: Number }
 
-  connect() {
-    console.log(this.refreshIntervalValue)
-    this.load()
+  // calls on page load, this will just reload the content from another html page anynchronously.
+  // connect() {
+  //   console.log(this.refreshIntervalValue)
 
-    if (this.hasRefreshIntervalValue) {
-      this.startRefreshing()
-    }
-  }
+  //   this.load()
 
-  disconnect() {
-    this.stopRefreshing()
-  }
+  //   if (this.hasRefreshIntervalValue) {
+  //     this.startRefreshing()
+  //   }
+  // }
 
-  load() {
-    fetch(this.urlValue)
+  // called when page disconnected and it will clear the refresh Interval
+  // disconnect() {
+  //   this.stopRefreshing()
+  // }
+
+  // userdefined function which used fetch() to get another page content and populate on current page.
+  // load() {
+  //   fetch(this.urlValue)
+  //   .then(response => response.text())
+  //   // .then(html => document.getElementById("display_message_content").innerHTML = html)
+  //   .then(html => this.element.innerHTML = html)
+  // }
+
+  // method accepts url defined under DOM `data-content-loader-url-param` on click event `data-action="content-loader#load"` triggered 
+  load({ params: { url } }) {
+    fetch(url)
       .then(response => response.text())
-      // .then(html => document.getElementById("display_message_content").innerHTML = html)
       .then(html => this.element.innerHTML = html)
   }
 
-  startRefreshing() {
-    this.refreshTimer = setInterval(() => {
-      this.load()
-    }, this.refreshIntervalValue)
-  }
+  // startRefreshing() {
+  //   this.refreshTimer = setInterval(() => {
+  //     this.load()
+  //   }, this.refreshIntervalValue)
+  // }
 
-  stopRefreshing() {
-    if (this.refreshTimer) {
-      clearInterval(this.refreshTimer)
-    }
-  }
+  // stopRefreshing() {
+  //   if (this.refreshTimer) {
+  //     clearInterval(this.refreshTimer)
+  //   }
+  // }
 }
